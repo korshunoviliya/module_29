6f71db43e7b227a96326a2cd49ae8ae99ca65fff
korshunoviliya <korshunov80@mail.ru>
2022-02-02 13:27:25 +0300

    task module 29 is ready

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 
+class Cat;
+
+class Animal
+{
+public:
+    virtual void say(Animal* a, Animal* b) = 0;
+};
+
+class Dog : public Animal
+{
+public:
+    virtual void say(Animal* a, Animal* b) override {
+        if(dynamic_cast<Dog*>(b)) {
+            std::cout << " Woof ";
+        }
+        else std::cout << " Bark ";
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    virtual void say(Animal* a, Animal* b) override {
+        if(dynamic_cast<Cat*>(b)) {
+            std::cout << " Purr ";
+        }
+        else std::cout << " Meow ";
+    }
+};
+
+void meeting(Animal* a, Animal* b){
+    a->say(a, b);
+    b->say(b, a);
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //Animal* a = new Dog();
+    //Animal* b = new Dog();
+
+    //Animal* a = new Dog();
+    //Animal* b = new Cat();
+
+    //Animal* a = new Cat();
+    //Animal* b = new Dog();
+
+    Animal* a = new Cat();
+    Animal* b = new Cat();
+    meeting(a, b);
+
     return 0;
 }
